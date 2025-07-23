f438eeebaf2606a178097f411034048d36b2551f
Rugved Pednekar <84618114+Rugvedpednekar@users.noreply.github.com>
2025-07-23 18:34:02 +0530

    Create display_resume.php

---
+++ display_resume.php
@@ -0,0 +1,21 @@
+<?php
+//Database Connection
+$servername = "localhost";
+$username ="root";
+$password="";
+$dbname = "job";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
